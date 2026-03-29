406cdade8cf1405a8f372b6c318020611e6c54a1
Suyog1707 <148788280+Suyog1707@users.noreply.github.com>
2026-03-29 12:58:57 +0530

    add user channel profile and watch history retrieval functionalities; update routes accordingly

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -8,7 +8,9 @@ import {
     updateUserDetails,
     getCurrentUser,
     updateAvatar,
-    updateCoverImage
+    updateCoverImage,
+    getUserChannelProfile,
+    getUserWatchHistory
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -37,16 +39,18 @@ router.route("/logout").post(verifyJWT, logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/get-current-user").get(verifyJWT, getCurrentUser)
-router.route("/update-details").post(verifyJWT, updateUserDetails)
-router.route("/update-avatar").post(
+router.route("/update-details").patch(verifyJWT, updateUserDetails)
+router.route("/update-avatar").patch(
     verifyJWT,
     upload.single("avatar"),
     updateAvatar
 )
-router.route("/update-cover-image").post(
+router.route("/update-cover-image").patch(
     verifyJWT,
     upload.single("coverImage"),
     updateCoverImage
 )
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT, getUserWatchHistory)
 
 export default router
