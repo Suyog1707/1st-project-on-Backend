39a54e311fc2239a9fb39dbc121c0dd8b836dc8e
Suyog Aldar <suyogaldar07@gmail.com>
2026-04-15 21:48:35 +0530

    implement addComment functionality and update comment controller imports

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,31 +1,51 @@
 import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { Comment } from "../models/comment.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+    const { videoId } = req.params
+    const { page = 1, limit = 10 } = req.query
 
 })
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
+    const { content } = req.body
+    const { videoId } = req.params
+
+    if (!content) {
+        throw new ApiError(400, "Required comment")
+    }
+
+    const owner = await User.findById(req.user?._id).select("-password -refreshToken -email -coverImage -watchHistory")
+
+    const comment = await Comment.create({
+        content,
+        video: videoId,
+        owner,
+    })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, comment, "Comment added Successfully")
+    )
 })
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
+    
 })
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
+    
 })
 
 export {
-    getVideoComments, 
-    addComment, 
+    getVideoComments,
+    addComment,
     updateComment,
-     deleteComment
-    }
+    deleteComment
+}
